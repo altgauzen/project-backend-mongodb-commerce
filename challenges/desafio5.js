22da43a132884f2c2f32ec7a12db97a96eac9465
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-11-24 18:13:38 +0000

    desafios 5 e 6 refatorados

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,3 +1,5 @@
-db.produtos.updateMany({ nome: { $not: { $eq: "McChicken" } } }, { $addToSet: { "ingredientes": "ketchup" } });
+db.produtos.updateMany(
+  { nome: { $not: { $eq: "McChicken" } } },
+  { $addToSet: { ingredientes: "ketchup" } });
 
 db.produtos.find({}, { _id: false, nome: 1, ingredientes: 1 });
